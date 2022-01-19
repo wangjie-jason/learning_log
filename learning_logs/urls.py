cbd7f5883c86249cc2b9402ab91647b543d8960f
wangjie-jason <1641540482@qq.com>
2022-01-19 14:19:54 +0800

    添加主题条目及条目详情页跳转

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,9 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # 主页
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # 显示所有的主题
+    path('topics/', views.topics, name='topics'),
+    # 特定主题的详细页面
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from .models import Topic
+
 
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """显示单个主题及其所有条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
