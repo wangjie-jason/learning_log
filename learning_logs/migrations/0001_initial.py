01022ddfad4369be30bee18b4b13ac13d1c081c4
wangjie-jason <1641540482@qq.com>
2022-01-22 20:14:36 +0800

    迁移数据库

--- learning_logs/migrations/0001_initial.py
+++ learning_logs/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Topioc',
+            name='Topic',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.CharField(max_length=200)),
